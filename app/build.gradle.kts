610b6f0d6b73b17bbd426174e3c0e880f84bd021
luisesc20230639 <20230639@ricaldone.edu.sv>
2024-07-11 00:41:14 -0600

    Activity rastrear paquetes y bottom sheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.material:material:1.13.0-alpha03")
 }
