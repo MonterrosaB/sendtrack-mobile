0c0e499617a0c3bd9d221b95c973dc0da92fbd35
luisesc20230639 <20230639@ricaldone.edu.sv>
2024-07-14 20:00:32 -0600

    splashScreen

---
+++ app/src/main/java/ptc24/st/sendtrack/splashScreen.kt
@@ -0,0 +1,33 @@
+package ptc24.st.sendtrack
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        GlobalScope.launch(Dispatchers.Main){
+            delay(1000)
+            val intent = Intent(this@splashScreen, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
